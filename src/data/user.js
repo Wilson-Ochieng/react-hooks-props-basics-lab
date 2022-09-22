99008737bbdcea4270ea4cfa226b8bc831d8c678
Wilson-Ochieng <wilsonochieng718@gmail.com>
2022-09-22 08:10:57 +0300

    Completed Assignment

--- src/data/user.js
+++ src/data/user.js
@@ -1,11 +1,11 @@
 const user = {
-  name: "Liza",
-  city: "New York",
+  name: "Wilson",
+  city: "Kenya",
   bio: "I made this!",
   color: "firebrick",
   links: {
-    github: "https://github.com/liza",
-    linkedin: "https://www.linkedin.com/in/liza/",
+    github: "https://github.com/wilson",
+    linkedin: "https://www.linkedin.com/in/Wilson/",
   },
 };
 
